4e2e257d93ac40ac6448f976e7910f9e6bc2b1ba
Guiselle Mejia <guissmejia@gmail.com>
2020-11-06 21:33:08 -0500

    The header and its navigation menu are created, the automatic slide section is created using JavaScript

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,47 @@
-console.log('Hi!')
+//Slide
+
+let slideList = document.querySelector(".Slide--list");
+
+let slideItem = document.querySelectorAll(".Slide__card");
+
+let counter = 1;
+
+let sizeItem = slideItem[0].clientWidth;
+console.log(sizeItem)
+
+let interval = 3500;
+
+window.addEventListener('resize', () => {
+  let sizeItem = slideItem[0].clientWidth;
+})
+
+setInterval(() => {
+  slide()
+}, interval)
+
+
+const slide = () => { 
+
+slideList.style.transform = "translate("+(-sizeItem*counter)+"px)";
+slideList.style.transition = "all .8s"
+
+  counter ++; 
+
+  if(counter == slideItem.length){
+    setTimeout(() => {
+      slideList.style.transform = "translate(0px)"
+      slideList.style.transition = "all .5s"
+      counter = 1
+    }, interval)
+  }
+}
+
+// Fetch to load data
+
+const moviesDataUrl = './data/moviesData.json'
+
+fetch(moviesDataUrl)
+.then(response => response.json())
+.then(response => {
+  console.log(response)
+})
